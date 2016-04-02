69cfb1fde6efccf89bd9bc224bae52b20521ae6a
Christian Ascone <ascone.christian@gmail.com>
2016-04-02 13:23:39 +0200

    First Homework

--- shared_data/christian/homework_es1/draw_param_curve.c
+++ shared_data/christian/homework_es1/draw_param_curve.c
@@ -20,9 +20,9 @@ void get_scale (RECT r1, RECT r2, float *scx, float *scy)
 	*scy = (r1.ymax - r1.ymin) / (r2.ymax - r2.ymin);
 }
 
-void wind_view (float px, float py, float *ix, float *iy, VIEWPORT view, WINDOW win)
+void wind_view (float px, float py, int *ix, int *iy, VIEWPORT view, WINDOW win)
 {
-float scx, scy;
+	float scx, scy;
 	get_scale (view, win, &scx, &scy);
 	*ix = 0.5 + scx * (px - win.xmin) + view.xmin;
 	*iy = 0.5 + scy * (win.ymin - py) + view.ymax;
@@ -30,35 +30,30 @@ float scx, scy;
 
 void assi (SDL_Renderer* ren, VIEWPORT view, WINDOW win)
 {
-float ipx, ipy;
+	int ipx, ipy;
 	wind_view (0, 0, &ipx, &ipy, view, win);
-        SDL_RenderDrawLine(ren, ipx, view.ymin, ipx, view.ymax);
+	SDL_RenderDrawLine(ren, ipx, view.ymin, ipx, view.ymax);
 	SDL_RenderDrawLine(ren, view.xmin, ipy, view.xmax, ipy);
 }
 
-void draw_data (SDL_Renderer* ren, VIEWPORT view, WINDOW win)
+void draw_data (SDL_Renderer* ren, int n, VIEWPORT view, WINDOW win, float x[], float y[])
 {
-	// curva parametrica definita da una funzione con parametro t che va da 0 a 1
-float x1, x2, y1, y2;
-float t, px, py;
+	int i;
+	int x1, x2, y1, y2;
+	float px, py;
 
-	t = 0;
-	// Equazione è y = 2t e x = 3t
-	px = t*3;
-	py = t*9;
+	px = x[1];
+	py = y[1];
 	wind_view (px, py, &x1, &y1, view, win);
 
-	for (t = 0; t <= 1; t+=0.1)
+	for (i = 2; i <= n; i++)
 	{
-		px = t*3;
-		py = t*2;
+		px = x[i];
+		py = y[i];
 
-		printf("px %f e py %f\n", px, py);
 
 		wind_view (px, py, &x2, &y2, view, win);
 		SDL_RenderDrawLine(ren, x1, y1, x2, y2);
-
-		printf("x1 %f e x2 %f \n y1 %f e y2 %f \n", x1, x2, y1, y2);
 
 		x1 = x2;
 		y1 = y2;
@@ -67,17 +62,60 @@ float t, px, py;
 	//assi (ren, view, win);
 }
 
+/*
+ * Esegue il set dei dati e della Window 
+**/
+void setData (int n, WINDOW* rect, float x[], float y[])
+{
+	int i;
+	float t, px, py;
+	t = 0;
+	// Equazione parametrica definita come segue
+	// x = t - 2*(sin(t))
+	// y = 2 - 2*(cos(t))
+	// 0 <= t <= 1
+	printf("*****************\n");
+	printf("* STAMPA VALORI *\n");
+	for (i = 0; i < n; i++){
+		px = t - 2*(sin(t));
+		py = 2 - 2*(cos(t));
+
+		printf("px[%d] %f\n", i, px);
+		printf("py[%d] %f\n", i, py);
+		x[i]=px;
+		y[i]=py;
+		t+=0.1;
+		//printf("%s\n", );
+	}
+	printf("*  FINE STAMPA  *\n");
+
+
+	rect->xmin=rect->xmax=x[0];
+	rect->ymin=rect->ymax=y[0];
+
+	for (i = 1; i < n; i++)
+	{
+		if(x[i]<rect->xmin) rect->xmin=x[i];
+		else if(x[i]>rect->xmax){ 
+			rect->xmax=x[i];	}
+
+		if(y[i]<rect->ymin) rect->ymin=y[i];
+		else if(y[i]>rect->ymax) rect->ymax=y[i];
+	}
+}
 
 int main (void)
 {
-SDL_Window *win;
-SDL_Renderer *ren;
+	SDL_Window *win;
+	SDL_Renderer *ren;
 //SDL_Texture *tex;
-SDL_Rect sub_v, v;
-TTF_Font *font;
-WINDOW fun_win;
-VIEWPORT fun_view;
-char ch;
+	SDL_Rect sub_v, v;
+	int n;
+	float x[DIM], y[DIM];
+	TTF_Font *font;
+	WINDOW fun_win;
+	VIEWPORT fun_view;
+	char ch;
 
 	if (SDL_Init (SDL_INIT_VIDEO) < 0)
 	{
@@ -125,11 +163,14 @@ char ch;
 
 
 //SDL_SetRenderDrawColor(ren, 240, 240, 240, 255);
-        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 	SDL_RenderFillRect (ren, &v);
-        SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
-	GC_DrawText (ren, font, 255, 0, 0, 0, 255, 255, 255, 0, "Grafico Polilinea", 600, 50, shaded);
-        SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
+	GC_DrawText (ren, font, 255, 0, 0, 0, 255, 255, 255, 0, "Grafico Curva", 600, 50, shaded);
+	GC_DrawText (ren, font, 0, 0, 0, 0, 255, 255, 255, 0, "x = t - 2*(sin(t))", 600, 90, shaded);
+	GC_DrawText (ren, font, 0, 0, 0, 0, 255, 255, 255, 0, "y = 2 - 2*(cos(t))", 600, 120, shaded);
+	GC_DrawText (ren, font, 0, 0, 0, 0, 255, 255, 255, 0, "0<=t<=1", 600, 150, shaded);
+	SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
 
 	sub_v.x = v.x + 10;
 	sub_v.y = v.y + 10;
@@ -146,14 +187,18 @@ char ch;
 //	SDL_RenderCopy(ren, tex, NULL, NULL);
 	SDL_RenderPresent(ren);
 
+	// Numero di valori da disegnare
+	n = 10;
+
 	do
 	{
+		setData (n, &fun_win, x, y);
 
-     SDL_SetRenderDrawColor(ren, 240, 240, 240, 255);
+		SDL_SetRenderDrawColor(ren, 240, 240, 240, 255);
 		SDL_RenderFillRect (ren, &sub_v);
 
-                SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
-		draw_data (ren, fun_view, fun_win);
+		SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
+		draw_data (ren, n, fun_view, fun_win, x, y);
 
 		//tex = SDL_CreateTextureFromSurface(ren, screen);
 		//SDL_RenderClear(ren);
