993d503e805a36875fffb57c8e5a1f34728bcd3a
Christian Ascone <ascone.christian@gmail.com>
2016-07-11 23:38:43 +0200

    Billboard with texture and images

--- shared_data/christian/progettoCar/car.cpp
+++ shared_data/christian/progettoCar/car.cpp
@@ -276,11 +276,16 @@ void drawBillboard () {
   }
   else {
     if (usecolor) glColor3f(0.5, 0.5, 0.5);   // colore rosso, da usare con Lighting
-    if (usecolor) SetupEnvmapTextureDecorMetal();
+    //if (usecolor) SetupEnvmapTextureDecorMetal();
+    glBindTexture(GL_TEXTURE_2D, 10);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   }
-  billboard_base.RenderNxF();
-  billboard_internal.RenderNxF();
-  billboard_lightsupport.RenderNxF();
+  billboard_base.RenderNxV();
+  billboard_internal.RenderNxV();
+  billboard_lightsupport.RenderNxV();
   //if (usecolor) glEnable(GL_LIGHTING);
 
   if (!useEnvmap)
